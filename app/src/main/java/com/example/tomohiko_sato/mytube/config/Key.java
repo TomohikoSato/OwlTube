6659a9c8cb589cc5b3c9211e9423a86c636e99f2
tomohiko_sato <tomohikosato1@gmail.com>
2016-11-10 20:55:56 +0900

    YoutubeAPIKey用のプレースホルダを追加

---
+++ app/src/main/java/com/example/tomohiko_sato/mytube/config/Key.java
@@ -0,0 +1,7 @@
+package com.example.tomohiko_sato.mytube.config;
+
+public class Key {
+	public static class Youtube {
+		public static final String API_KEY = ""; // fill your youtube api key
+	}
+}

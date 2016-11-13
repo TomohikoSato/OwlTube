e5306048047c9500c619087366d8d38dbaaa79b7
tomohiko_sato <tomohikosato1@gmail.com>
2016-11-13 21:57:18 +0900

    ViewPager作成中

--- app/src/main/java/com/example/tomohiko_sato/mytube/presentation/MainActivity.java
+++ app/src/main/java/com/example/tomohiko_sato/mytube/presentation/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.tomohiko_sato.mytube.presentation;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,11 +12,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.tomohiko_sato.mytube.R;
+import com.example.tomohiko_sato.mytube.presentation.dummy.DummyContent;
 
 /**
  * 起動後最初に表示されるメインの画面
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TopFragment.OnTopFragmentInteractionListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,43 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
 		setSupportActionBar(toolbar);
+
+		ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+		viewPager.setAdapter(new SectionPagerAdapter(getSupportFragmentManager()));
+	}
+
+	@Override
+	public void onTopFragmentInteraction(DummyContent.DummyItem item) {
+
+	}
+
+	static class SectionPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return TopFragment.newInstance(1);
+		}
+
+		@Override
+		public int getCount() {
+			return 2;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+				case 0:
+					return "Top";
+				case 1:
+					return "Recent";
+			}
+			return null;
+		}
+
 	}
 
 	@Override

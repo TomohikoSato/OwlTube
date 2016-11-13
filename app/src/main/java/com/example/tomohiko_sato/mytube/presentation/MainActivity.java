af0b2cff6e1e099961f71022e7085872151f26e5
tomohiko_sato <tomohikosato1@gmail.com>
2016-11-14 00:01:02 +0900

    なんとなくViewPager

--- app/src/main/java/com/example/tomohiko_sato/mytube/presentation/MainActivity.java
+++ app/src/main/java/com/example/tomohiko_sato/mytube/presentation/MainActivity.java
@@ -9,11 +9,15 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.tomohiko_sato.mytube.R;
 import com.example.tomohiko_sato.mytube.presentation.dummy.DummyContent;
+
+import java.util.zip.Inflater;
 
 /**
  * 起動後最初に表示されるメインの画面
@@ -27,15 +31,22 @@ public class MainActivity extends AppCompatActivity implements TopFragment.OnTop
 		Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
 		setSupportActionBar(toolbar);
 
+		LayoutInflater inflater = LayoutInflater.from(this);
+
 		ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
 		viewPager.setAdapter(new SectionPagerAdapter(getSupportFragmentManager()));
 
-		TabLayout tabLayout= (TabLayout) findViewById(R.id.tab_layout);
-
-		tabLayout.addTab(tabLayout.newTab().setText("tab 1"));
-		tabLayout.addTab(tabLayout.newTab().setText("tab 2"));
+		TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+		/*
+		View tabItem = inflater.inflate(R.layout.tab_item, null);
+		tabLayout.addTab(tabLayout.newTab().setCustomView(tabItem));
+		tabLayout.addTab(tabLayout.newTab().setCustomView(tabItem));
+		*/
 
 		tabLayout.setupWithViewPager(viewPager);
+
+		tabLayout.getTabAt(0).setText("Top").setIcon(R.drawable.top);
+		tabLayout.getTabAt(1).setText("Recent").setIcon(R.drawable.recent);
 	}
 
 	@Override
@@ -59,18 +70,18 @@ public class MainActivity extends AppCompatActivity implements TopFragment.OnTop
 			return 2;
 		}
 
-		@Override
+/*		@Override
 		public CharSequence getPageTitle(int position) {
 
 			switch (position) {
 				case 0:
-					return "Top";
+					return "hoge";
 				case 1:
-					return "Recent";
+					return "fuga";
 			}
 
 			return null;
-		}
+		}*/
 	}
 
 	@Override

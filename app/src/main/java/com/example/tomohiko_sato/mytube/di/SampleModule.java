0ab8de30476ddbb4edc318259fc01cd054fd6d91
tomohiko sato <tomohikosato1@gmail.com>
2016-11-20 19:16:44 +0900

    DIしてる

--- app/src/main/java/com/example/tomohiko_sato/mytube/di/SampleModule.java
+++ app/src/main/java/com/example/tomohiko_sato/mytube/di/SampleModule.java
@@ -1,6 +1,7 @@
 package com.example.tomohiko_sato.mytube.di;
 
 import com.example.tomohiko_sato.mytube.domain.popular.PopularUseCase;
+import com.example.tomohiko_sato.mytube.infra.api.youtube.YoutubeRequest;
 
 import dagger.Module;
 import dagger.Provides;
@@ -9,7 +10,12 @@ import dagger.Provides;
 public class SampleModule {
 
 	@Provides
-	PopularUseCase providePopularUseCase () {
-		return new PopularUseCase();
+	PopularUseCase providePopularUseCase (YoutubeRequest youtubeRequest) {
+		return new PopularUseCase(youtubeRequest);
+	}
+
+	@Provides
+	YoutubeRequest provideYoutubeRequest () {
+		return new YoutubeRequest();
 	}
 }
--- app/src/main/java/com/example/tomohiko_sato/mytube/domain/popular/PopularUsecase.java
+++ app/src/main/java/com/example/tomohiko_sato/mytube/domain/popular/PopularUsecase.java
@@ -3,10 +3,19 @@ package com.example.tomohiko_sato.mytube.domain.popular;
 import com.example.tomohiko_sato.mytube.infra.api.youtube.YoutubeRequest;
 import com.example.tomohiko_sato.mytube.infra.api.youtube.data.popular.Popular;
 
+import javax.inject.Inject;
+
 import retrofit2.Callback;
 
 public class PopularUseCase {
+	YoutubeRequest youtubeRequest;
+
+	@Inject
+	public PopularUseCase(YoutubeRequest youtubeRequest) {
+		this.youtubeRequest = youtubeRequest;
+	}
+
 	public void fetchPopular(Callback<Popular> callback) {
-		new YoutubeRequest().fetchPopular(callback);
+		youtubeRequest.fetchPopular(callback);
 	}
 }

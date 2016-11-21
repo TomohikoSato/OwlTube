cc1c3b55cf872cb77584774f1a290a29ccf59c16
tomohiko sato <tomohikosato1@gmail.com>
2016-11-21 20:45:15 +0900

    Fragmentでなんかハマる

--- app/src/main/java/com/example/tomohiko_sato/mytube/presentation/search/SearchHistoryRecyclerViewAdapter.java
+++ app/src/main/java/com/example/tomohiko_sato/mytube/presentation/search/SearchHistoryRecyclerViewAdapter.java
@@ -14,8 +14,7 @@ import java.util.List;
 /**
  * {@link RecyclerView.Adapter} that can display a {@link String} and makes a call to the
  * specified {@link OnSearchHistoryFragmentInteractionListener}.
- * TODO: Replace the implementation with code for your data type.
- */
+  */
 public class SearchHistoryRecyclerViewAdapter extends RecyclerView.Adapter<SearchHistoryRecyclerViewAdapter.ViewHolder> {
 
 	private final List<String> searchHistories;

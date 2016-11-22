aaac03799c86df5485fa94bdb64438618c6c6f2b
tomohiko sato <tomohikosato1@gmail.com>
2016-11-23 01:57:54 +0900

    命名修正

--- app/src/main/java/com/example/tomohiko_sato/owltube/presentation/search/SearchHistoryRecyclerViewAdapter.java
+++ app/src/main/java/com/example/tomohiko_sato/owltube/presentation/search/SearchHistoryRecyclerViewAdapter.java
@@ -33,7 +33,7 @@ public class SearchHistoryRecyclerViewAdapter extends RecyclerView.Adapter<Searc
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		View view = LayoutInflater.from(parent.getContext())
-				.inflate(R.layout.fragment_search_history_item, parent, false);
+				.inflate(R.layout.list_item_search_history, parent, false);
 		return new ViewHolder(view);
 	}
 

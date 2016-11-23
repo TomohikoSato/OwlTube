582dab5202c24f573e23f325521bb5a9d7657119
tomohiko sato <tomohikosato1@gmail.com>
2016-11-23 12:37:28 +0900

    @IntDef 使ってみた

--- app/src/main/java/com/example/tomohiko_sato/owltube/presentation/player/PlayerRecyclerViewAdapter.java
+++ app/src/main/java/com/example/tomohiko_sato/owltube/presentation/player/PlayerRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.tomohiko_sato.owltube.presentation.player;
 
+import android.support.annotation.IntDef;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,34 +9,36 @@ import android.view.ViewGroup;
 import com.example.tomohiko_sato.owltube.R;
 import com.example.tomohiko_sato.owltube.presentation.common_component.VideoItemRecyclerViewHolder;
 
-public class PlayerRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-	enum ViewType {
-		Header(0),
-		Body(1),
-		Footer(2);
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
-		private final int viewType;
-		private ViewType(final int viewType) {
-			this.viewType = viewType;
-		}
-		public int getViewType() {
-			return this.viewType;
-		}
+public class PlayerRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+	private static final int VIEW_TYPE_HEADER = 0;
+	private static final int VIEW_TYPE_BODY = 1;
+	private static final int VIEW_TYPE_FOOTER = 2;
+
+	@IntDef(value = {VIEW_TYPE_HEADER, VIEW_TYPE_BODY, VIEW_TYPE_FOOTER})
+	@Retention(RetentionPolicy.SOURCE)
+	@interface ITEM_VIEW_TYPE {
 	}
 
 
 	@Override
-	public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+	public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, @ITEM_VIEW_TYPE int viewType) {
+		
 		View view = LayoutInflater.from(parent.getContext())
 				.inflate(R.layout.list_item_video, parent, false);
 		return new VideoItemRecyclerViewHolder(view);
 	}
 
 	@Override
+	@ITEM_VIEW_TYPE
 	public int getItemViewType(int position) {
 		if (position == 1) {
-			return ViewType.Header.getViewType();
+			return VIEW_TYPE_HEADER;
 		}
+		return VIEW_TYPE_BODY;
 	}
 
 	@Override

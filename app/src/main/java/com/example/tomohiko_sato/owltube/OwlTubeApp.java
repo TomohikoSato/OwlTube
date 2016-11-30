2a2b7263af19493f56f64d2608ae5e8e034d42af
tomohiko sato <tomohikosato1@gmail.com>
2016-11-30 22:51:55 +0900

    LeakCanaryを導入

--- app/src/main/java/com/example/tomohiko_sato/owltube/OwlTubeApp.java
+++ app/src/main/java/com/example/tomohiko_sato/owltube/OwlTubeApp.java
@@ -3,6 +3,7 @@ package com.example.tomohiko_sato.owltube;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
 
 
 public class OwlTubeApp extends Application {
@@ -11,5 +12,11 @@ public class OwlTubeApp extends Application {
 	public void onCreate() {
 		super.onCreate();
 		Stetho.initializeWithDefaults(this);
+		if (LeakCanary.isInAnalyzerProcess(this)) {
+			// This process is dedicated to LeakCanary for heap analysis.
+			// You should not init your app in this process.
+			return;
+		}
+		LeakCanary.install(this);
 	}
 }

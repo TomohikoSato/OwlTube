c1e87822af0ad522ef132b692d0b868b9c186422
tomohiko sato <tomohikosato1@gmail.com>
2016-12-31 02:27:00 +0900

    ログ追加した

--- app/src/main/java/com/example/tomohiko_sato/owltube/di/SampleModule.java
+++ app/src/main/java/com/example/tomohiko_sato/owltube/di/SampleModule.java
@@ -1,6 +1,7 @@
 package com.example.tomohiko_sato.owltube.di;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.example.tomohiko_sato.owltube.domain.player.PlayerUseCase;
 import com.example.tomohiko_sato.owltube.domain.popular.PopularUseCase;
@@ -24,6 +25,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 @Module
 public class SampleModule {
 	private final Context context;
+	private final static String TAG = SampleModule.class.getSimpleName();
+
 
 	public SampleModule(Context context) {
 		this.context = context;
@@ -31,37 +34,44 @@ public class SampleModule {
 
 	@Provides
 	Context provideContext() {
+		Log.d(TAG, "provicdeContext");
 		return context;
 	}
 
 	@Provides
 	PopularUseCase providePopularUseCase(YoutubeRequest youtubeRequest) {
+		Log.d(TAG, "provicdePopularUseCase");
 		return new PopularUseCase(youtubeRequest);
 	}
 
 	@Provides
 	RecentlyWatchedUseCase provideRecentlyWatchedUseCase(YoutubeRequest youtubeRequest, Context context, RecentlyWatchedDao recentlyWatchedDao) {
+		Log.d(TAG, "provicdeRecentlyWatchedUseCase");
 		return new RecentlyWatchedUseCase(youtubeRequest, context, recentlyWatchedDao);
 	}
 
 	@Provides
 	PlayerUseCase providePlayerUseCase(RecentlyWatchedDao dao, YoutubeRequest request) {
+		Log.d(TAG, "provicdePlayerUseCase");
 		return new PlayerUseCase(dao, request);
 	}
 
 
 	@Provides
 	SearchUseCase provideSearchUseCase(YoutubeRequest youtubeRequest, GoogleRequest googleRequest, SearchHistoryDao dao) {
+		Log.d(TAG, "provideSearchUseCase");
 		return new SearchUseCase(youtubeRequest, googleRequest, dao);
 	}
 
 	@Provides
 	YoutubeRequest provideYoutubeRequest(YoutubeAPI api) {
+		Log.d(TAG, "provideYoutubeRequest");
 		return new YoutubeRequest(api);
 	}
 
 	@Provides
 	YoutubeAPI provideYoutubeAPI() {
+		Log.d(TAG, "provideYoutubeAPI");
 		return new Retrofit.Builder()
 				.baseUrl("https://www.googleapis.com/youtube/v3/")
 				.addConverterFactory(GsonConverterFactory.create())
@@ -70,11 +80,13 @@ public class SampleModule {
 
 	@Provides
 	GoogleRequest provideGoogleRequest() {
+		Log.d(TAG, "provideGoogleRequest");
 		return new GoogleRequest();
 	}
 
 	@Provides
 	GoogleAPI provideGoogleAPI() {
+		Log.d(TAG, "provideGoogleAPI");
 		return new Retrofit.Builder()
 				.baseUrl("http://suggestqueries.google.com/complete/")
 				.addConverterFactory(GsonConverterFactory.create())
@@ -83,16 +95,19 @@ public class SampleModule {
 
 	@Provides
 	RecentlyWatchedDao provideRecentlyWatchedDao(DefaultDBHelper helper) {
+		Log.d(TAG, "provideRecentlyWatchedDao");
 		return new RecentlyWatchedDaoImpl(helper);
 	}
 
 	@Provides
 	DefaultDBHelper provideDefaultDBHelper() {
+		Log.d(TAG, "provideDefaultDBHelper");
 		return new DefaultDBHelper(context);
 	}
 
 	@Provides
 	SearchHistoryDao provideSearcHistoryDao (DefaultDBHelper helper) {
+		Log.d(TAG, "provideSearcHistoryDao");
 		return new SearchHistoryDaoImpl(helper);
 	}
 }

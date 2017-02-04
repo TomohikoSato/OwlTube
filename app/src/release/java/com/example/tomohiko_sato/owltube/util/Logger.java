ac98ab8c9e6b8a7e97cce861c81de803e60ad2d0
tomohiko sato <tomohikosato1@gmail.com>
2017-02-04 20:06:46 +0900

    releaseではログを出力しないように修正

---
+++ app/src/release/java/com/example/tomohiko_sato/owltube/util/Logger.java
@@ -0,0 +1,23 @@
+package com.example.tomohiko_sato.owltube.util;
+
+/**
+ * releaseではログを出力しない
+ * TODO: crashlytics等への通知
+ */
+public class Logger {
+	public static void d(String str, Object... args) {
+
+	}
+
+	public static void i(String str, Object... args) {
+
+	}
+
+	public static void w(String str, Object... args) {
+
+	}
+
+	public static void e(String str, Object... args) {
+
+	}
+}

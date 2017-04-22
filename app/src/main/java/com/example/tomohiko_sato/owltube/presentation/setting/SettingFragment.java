59f840a8fd750225eb1d57ce171500e4db10f869
tomohiko sato <tomohikosato1@gmail.com>
2017-04-23 02:06:07 +0900

    Settingが出るようにした

--- app/src/main/java/com/example/tomohiko_sato/owltube/presentation/setting/SettingFragment.java
+++ app/src/main/java/com/example/tomohiko_sato/owltube/presentation/setting/SettingFragment.java
@@ -1,7 +1,6 @@
 package com.example.tomohiko_sato.owltube.presentation.setting;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,14 +9,6 @@ import android.view.ViewGroup;
 
 import com.example.tomohiko_sato.owltube.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SettingFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SettingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingFragment extends Fragment {
 	public static final String TAG = SettingFragment.class.getSimpleName();
 
@@ -30,7 +21,6 @@ public class SettingFragment extends Fragment {
 	private String mParam1;
 	private String mParam2;
 
-	private OnFragmentInteractionListener mListener;
 
 	public SettingFragment() {
 		// Required empty public constructor
@@ -70,46 +60,18 @@ public class SettingFragment extends Fragment {
 		return inflater.inflate(R.layout.fragment_setting, container, false);
 	}
 
-	// TODO: Rename method, update argument and hook method into UI event
-	public void onButtonPressed(Uri uri) {
-		if (mListener != null) {
-			mListener.onFragmentInteraction(uri);
-		}
-	}
-
 	@Override
 	public void onAttach(Context context) {
 		super.onAttach(context);
-		if (context instanceof OnFragmentInteractionListener) {
-			mListener = (OnFragmentInteractionListener) context;
-		} else {
-			throw new RuntimeException(context.toString()
-					+ " must implement OnFragmentInteractionListener");
-		}
 	}
 
 	@Override
 	public void onDetach() {
 		super.onDetach();
-		mListener = null;
 	}
 
 	public static SettingFragment newInstance() {
-		return null;
+		return new SettingFragment();
 	}
 
-	/**
-	 * This interface must be implemented by activities that contain this
-	 * fragment to allow an interaction in this fragment to be communicated
-	 * to the activity and potentially other fragments contained in that
-	 * activity.
-	 * <p>
-	 * See the Android Training lesson <a href=
-	 * "http://developer.android.com/training/basics/fragments/communicating.html"
-	 * >Communicating with Other Fragments</a> for more information.
-	 */
-	public interface OnFragmentInteractionListener {
-		// TODO: Update argument type and name
-		void onFragmentInteraction(Uri uri);
-	}
 }

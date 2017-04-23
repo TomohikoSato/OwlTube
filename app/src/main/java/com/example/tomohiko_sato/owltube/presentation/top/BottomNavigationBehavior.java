c7e80faa875c6c1697dd9129d31490f574cb2d80
tomohiko sato <tomohikosato1@gmail.com>
2017-04-23 20:16:30 +0900

    不要なので削除

--- app/src/main/java/com/example/tomohiko_sato/owltube/presentation/top/BottomNavigationBehavior.java
+++ app/src/main/java/com/example/tomohiko_sato/owltube/presentation/top/BottomNavigationBehavior.java
@@ -2,28 +2,16 @@ package com.example.tomohiko_sato.owltube.presentation.top;
 
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPropertyAnimatorCompat;
-import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.View;
-import android.view.animation.Interpolator;
 
 /**
  * Created by Nikola D. on 3/15/2016.
  */
-public final class BottomNavigationBehavior<V extends View> extends VerticalScrollingBehavior<V> {
-	private static final Interpolator INTERPOLATOR = new LinearOutSlowInInterpolator();
-	private boolean hidden = false;
-	private ViewPropertyAnimatorCompat mOffsetValueAnimator;
-	private boolean scrollingEnabled = true;
-	int[] attrsArray = new int[]{
-			android.R.attr.id, android.R.attr.elevation};
-	private int mElevation = 8;
+public final class BottomNavigationBehavior extends CoordinatorLayout.Behavior<BottomNavigationView> {
 
 	public BottomNavigationBehavior() {
 		super();
@@ -31,52 +19,23 @@ public final class BottomNavigationBehavior<V extends View> extends VerticalScro
 
 	public BottomNavigationBehavior(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		TypedArray a = context.obtainStyledAttributes(attrs,
-				attrsArray);
-		mElevation = a.getResourceId(1, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, mElevation, context.getResources().getDisplayMetrics()));
-		a.recycle();
 	}
 
 	@Override
-	public void onNestedVerticalOverScroll(CoordinatorLayout coordinatorLayout, V child, @ScrollDirection int direction, int currentOverScroll, int totalOverScroll) {
+	public boolean onStartNestedScroll(
+			CoordinatorLayout coordinatorLayout, BottomNavigationView child,
+			View directTargetChild, View target, int nestedScrollAxes) {
+		return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
 	}
 
 	@Override
-	public void onDirectionNestedPreScroll(CoordinatorLayout coordinatorLayout, V child, View target, int dx, int dy, int[] consumed, @ScrollDirection int scrollDirection) {
-		handleDirection(child, scrollDirection);
-	}
-
-	private void handleDirection(V child, @ScrollDirection int scrollDirection) {
-		if (!scrollingEnabled) return;
-		if (scrollDirection == ScrollDirection.SCROLL_DIRECTION_DOWN && hidden) {
-			hidden = false;
-			animateOffset(child, 0);
-		} else if (scrollDirection == ScrollDirection.SCROLL_DIRECTION_UP && !hidden) {
-			hidden = true;
-			animateOffset(child, child.getHeight());
-		}
-	}
-
-	@Override
-	protected boolean onNestedDirectionFling(CoordinatorLayout coordinatorLayout, V child, View target, float velocityX, float velocityY, @ScrollDirection int scrollDirection) {
-		handleDirection(child, scrollDirection);
-		return true;
-	}
-
-	private void animateOffset(final V child, final int offset) {
-		ensureOrCancelAnimator(child);
-		mOffsetValueAnimator.translationY(offset).start();
-	}
-
-
-	private void ensureOrCancelAnimator(@NonNull V child) {
-		if (mOffsetValueAnimator == null) {
-			mOffsetValueAnimator = ViewCompat.animate(child);
-			mOffsetValueAnimator.setDuration(100);
-			mOffsetValueAnimator.setInterpolator(INTERPOLATOR);
+	public void onNestedScroll(CoordinatorLayout coordinatorLayout,
+							   BottomNavigationView child, View target, int dxConsumed, int dyConsumed,
+							   int dxUnconsumed, int dyUnconsumed) {
+		if (dyConsumed > 0) {
+//			Logger.e("hide");
 		} else {
-			mOffsetValueAnimator.cancel();
+//			Logger.e("show");
 		}
 	}
-
 }
